bcf15ebc11d884bd157d6ee33f73c95b3c0e3fe8
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-09-01 19:51:43 -0300

    Start Project

---
+++ src/main/java/com/learnwebservices/learn/LearnWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.learnwebservices.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnWebservicesApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/learnwebservices/learn/LearnWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwebservices.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
