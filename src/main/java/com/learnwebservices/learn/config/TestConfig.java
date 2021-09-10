046ae4007272066c4d5e018d7d0a53e6b22e0ce2
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-09-10 19:06:29 -0300

    Payment, one-to-one association

--- src/main/java/com/learnwebservices/learn/config/TestConfig.java
+++ src/main/java/com/learnwebservices/learn/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.learnwebservices.learn.entities.Category;
 import com.learnwebservices.learn.entities.Order;
 import com.learnwebservices.learn.entities.OrderItem;
+import com.learnwebservices.learn.entities.Payment;
 import com.learnwebservices.learn.entities.Product;
 import com.learnwebservices.learn.entities.User;
 import com.learnwebservices.learn.entities.enums.OrderStatus;
@@ -66,6 +67,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-22T22:23:22Z"), o1);
+		o1.setPayment(pay1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		categoryRepository.saveAll(Arrays.asList(c1, c2));
