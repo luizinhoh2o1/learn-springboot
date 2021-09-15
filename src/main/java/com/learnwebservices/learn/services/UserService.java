247115c3988c23f950606ae33831455857efb022
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-09-14 21:15:03 -0300

    Exception handling - delete Pt2

--- src/main/java/com/learnwebservices/learn/services/UserService.java
+++ src/main/java/com/learnwebservices/learn/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.learnwebservices.learn.entities.User;
 import com.learnwebservices.learn.repositories.UserRepository;
+import com.learnwebservices.learn.services.exceptions.DatabaseException;
 import com.learnwebservices.learn.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,17 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
